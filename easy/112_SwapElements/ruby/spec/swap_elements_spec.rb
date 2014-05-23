ad020d1e54bdd52b1db0a04f61b07fbcb551bb90
tomszymanski <szymanski.tom@gmail.com>
2014-05-23 08:10:03 -0600

    Easy: 112: SwapElements: Ruby: Success

---
+++ easy/112_SwapElements/ruby/spec/swap_elements_spec.rb
@@ -0,0 +1,51 @@
+require_relative '../swap_elements'
+
+describe "swap_elements" do
+  describe "ParseFile" do
+    let(:parse_file){ParseFile.new('1 2 3 : 0-1, 1-2')}
+
+    describe "#numbers_array" do
+      it "returns ['1','2','3']" do
+        expect(parse_file.numbers_array).to eq ["1", "2", "3"]
+      end
+    end
+
+    describe "#actions_array" do
+      it "returns [['0','1'],['1','2']]" do
+        expect(parse_file.actions_array).to eq [["0","1"],["1","2"]]
+      end
+    end
+  end
+
+  describe "SwapArrayValues" do
+    let(:swap_array_values) {SwapArrayValues.new(["1","2","3"],[["0","1"],["1","2"]])}
+
+    describe "#swap" do
+      it "returns ['2','1','3'] when swapping indices 0 and 1" do
+        expect(swap_array_values.swap(["1","2","3"], 0, 1)).to eq ["2", "1", "3"]
+      end
+    end
+
+    describe "#update" do
+      it "returns '2 3 1' given '1 2 3', [['0','1'],['1','2']] " do
+        expect(swap_array_values.update).to eq "2 3 1"
+      end
+    end
+  end
+
+  describe "SwapElements" do
+    before do
+      $stdout = StringIO.new
+    end
+
+    after(:all) do
+      $stdout = STDOUT
+    end
+
+    it "should return correct test outputs given test input" do
+      SwapElements.new('../../test_input.txt')
+      expect($stdout.string).to match('9 2 3 4 5 6 7 8 1')
+      expect($stdout.string).to match('2 4 3 1 5 6 7 8 9 10')
+    end
+  end
+end
---
+++ easy/112_SwapElements/ruby/swap_elements.rb
@@ -0,0 +1,76 @@
+class ParseFile
+  def initialize(string_to_parse)
+    @string_to_parse = string_to_parse
+  end
+
+  def numbers_array
+    number_string = split_numbers_and_actions(@string_to_parse)[:number_string]
+    number_string.split
+  end
+
+  def actions_array
+    action_string = split_numbers_and_actions(@string_to_parse)[:action_string]
+    split_action_string(action_string)
+  end
+
+  private
+
+  def split_numbers_and_actions(string_to_split)
+    split_string = string_to_split.split(":")
+    number_string = split_string[0]
+    action_string = split_string[1].delete(" ")
+    return {number_string: number_string, action_string: action_string}
+  end
+
+  def split_action_string(action_string)
+    return_array = Array.new
+    split_actions = action_string.split(",")
+    split_actions.each do |action|
+      return_array << action.split("-")
+    end
+    return_array
+  end
+end
+
+class SwapArrayValues
+  def initialize(numbers_array, actions_array)
+    @numbers_array = numbers_array
+    @actions_array = actions_array
+  end
+
+  def swap(array, first_index, second_index)
+    array[first_index], array[second_index] = array[second_index], array[first_index]
+    array
+  end
+
+  def update
+    @actions_array.each do |action|
+      @numbers_array = swap(@numbers_array, action[0].to_i, action[1].to_i)
+    end
+    array_to_string(@numbers_array)
+  end
+
+  private
+
+  def array_to_string(array_to_convert)
+    return_string = ""
+    array_to_convert.each do |number|
+      return_string += (number + " ")
+    end
+    return_string.rstrip
+  end
+end
+
+class SwapElements
+  def initialize(string_inputs = ARGV[0])
+    IO.foreach(string_inputs) do |line|
+      parsed_string = ParseFile.new(line)
+      numbers_array = parsed_string.numbers_array
+      actions_array = parsed_string.actions_array
+      swapped_array = SwapArrayValues.new(numbers_array, actions_array).update
+      puts swapped_array
+    end
+  end
+endgit 
+
+SwapElements.new
