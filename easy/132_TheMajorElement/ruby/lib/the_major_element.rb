d2c17ba6d6a0861842e08fe9a7c43b1b5e554d66
tomszymanski <szymanski.tom@gmail.com>
2014-06-01 15:37:50 -0600

    Easy: 132: The Major Element: Ruby: 5sec Run :(

--- easy/132_TheMajorElement/ruby/lib/the_major_element.rb
+++ easy/132_TheMajorElement/ruby/lib/the_major_element.rb
@@ -2,15 +2,46 @@ class ParseInput
   def initialize(string_to_parse)
     @string_to_parse = string_to_parse
   end
+
+  def to_a
+    return_array = @string_to_parse.split(",")
+    return_array.map!(&:to_i)
+  end
+end
+
+class ComputeArray
+  def initialize(array_to_calc)
+    @array_to_calc = array_to_calc
+    array_length = array_to_calc.length
+    @half_array_length = array_length/2
+  end
+
+  def major_element
+    major_element = 'None'
+    @array_to_calc.uniq.each do |val|
+      if over_half?(val)
+        major_element = val.to_s
+        break
+      end
+    end
+    major_element
+  end
+
+  private
+
+  def over_half?(val)
+    @array_to_calc.count(val) > @half_array_length
+  end
 end
 
 class TheMajorElement
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
       parsed_string = ParseInput.new(line.strip)
-      #doit
+      computed_array = ComputeArray.new(parsed_string.to_a)
+      puts computed_array.major_element
     end
   end
 end
 
-#TheMajorElement.new
+TheMajorElement.new
--- easy/132_TheMajorElement/ruby/spec/the_major_element_spec.rb
+++ easy/132_TheMajorElement/ruby/spec/the_major_element_spec.rb
@@ -2,6 +2,26 @@ require_relative 'spec_helper'
 
 RSpec.describe ParseInput do
   let(:parse_input) { ParseInput.new('92,19,19,76,19,21,19,85,19,19,19,94,19,19,22,67,83,19,19,54,59,1,19,19')}
+
+  describe "#to_a" do
+    it "should return an array of integers" do
+      expect(parse_input.to_a).to eq [92, 19, 19, 76, 19, 21, 19, 85, 19, 19, 19, 94, 19, 19, 22, 67, 83, 19, 19, 54, 59, 1, 19, 19]
+    end
+  end
+end
+
+RSpec.describe ComputeArray do
+  describe "#major_element" do
+    it "should return 'None' if there is no major element" do
+      compute_array = ComputeArray.new([1, 2, 3])
+      expect(compute_array.major_element).to eq 'None'
+    end
+
+    it "should return 1 when 2/3 is 1s" do
+      compute_array = ComputeArray.new([1, 1, 2])
+      expect(compute_array.major_element).to eq '1'
+    end
+  end
 end
 
 RSpec.describe TheMajorElement do
