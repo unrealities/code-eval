7b8752850ddd9fa1fc5dd27a4a105423b332e8ec
tomszymanski <szymanski.tom@gmail.com>
2014-05-31 21:52:31 -0600

    Easy: 136: Racing Chars: Success

--- easy/136_RacingChars/ruby/lib/racing_chars.rb
+++ easy/136_RacingChars/ruby/lib/racing_chars.rb
@@ -1,14 +1,45 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @string_to_parse = string_to_parse
+class Drive
+  def initialize(course, prev_location=nil)
+    @course = course
+    @prev_location = prev_location
+  end
+
+  def checkpoint
+    @course.index('C')
+  end
+
+  def gate
+    @course.index('_')
+  end
+
+  def path
+    checkpoint.nil? ? gate : checkpoint
+  end
+
+  def move
+    if (@prev_location.nil? || path == @prev_location)
+      '|'
+    elsif (path < @prev_location)
+      '/'
+    else
+      "\\"
+    end
+  end
+
+  def new_course
+    new_course = String.new(@course)
+    new_course[path] = move
+    new_course
   end
 end
 
 class RacingChars
   def initialize(string_inputs = ARGV[0])
+    prev_location = nil
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      #doit
+      drive = Drive.new(line, prev_location)
+      puts drive.new_course
+      prev_location = drive.path
     end
   end
 end
--- easy/136_RacingChars/ruby/spec/racing_chars_spec.rb
+++ easy/136_RacingChars/ruby/spec/racing_chars_spec.rb
@@ -1,7 +1,38 @@
 require_relative 'spec_helper'
 
-RSpec.describe ParseInput do
-  let(:parse_input) { ParseInput.new('86,2,3')}
+RSpec.describe Drive do
+  let(:drive) {Drive.new('########C_##')}
+
+  describe "#checkpoint" do
+    it "should return 8" do
+      expect(drive.checkpoint).to eq 8
+    end
+  end
+
+  describe "#gate" do
+    it "should return 9" do
+      expect(drive.gate).to eq 9
+    end
+  end
+
+  describe "#path" do
+    it "should return 8" do
+      expect(drive.path).to eq 8
+    end
+  end
+
+  describe "#move" do
+    it "should have a '|'" do
+      expect(drive.move).to eq '|'
+    end
+  end
+
+  describe "#new_course" do
+    it "should return the correctly updated course" do
+      expect(drive.new_course).to eq '########|_##'
+    end
+  end
+
 end
 
 RSpec.describe RacingChars do
@@ -16,16 +47,16 @@ RSpec.describe RacingChars do
   describe "#initialize" do
     it "should return correct test outputs given test input" do
       RacingChars.new('../test_input.txt')
-      expect($stdout.string).to match('#########|##')
+      expect($stdout.string).to match('#########\|##')
       expect($stdout.string).to match('########/_##')
       expect($stdout.string).to match('#######/####')
-      expect($stdout.string).to match('######_#\###')
-      expect($stdout.string).to match('#######_|###')
+      expect($stdout.string).to match(/######_#\\###/)
+      expect($stdout.string).to match('#######_\|###')
       expect($stdout.string).to match('#######/####')
       expect($stdout.string).to match('######/#_###')
-      expect($stdout.string).to match('######|_####')
-      expect($stdout.string).to match('#######\####')
-      expect($stdout.string).to match('#######|####')
+      expect($stdout.string).to match('######\|_####')
+      expect($stdout.string).to match(/#######\\####/)
+      expect($stdout.string).to match('#######\|####')
     end
   end
 end
