cc6c2b9f891d2bbe4f78485d25664b6f93f526c3
tomszymanski <szymanski.tom@gmail.com>
2014-06-04 17:18:29 -0600

    Easy: 22: Fibonacci Series: Ruby: Success

--- easy/22_FibonacciSeries/ruby/lib/fibonacci_series.rb
+++ easy/22_FibonacciSeries/ruby/lib/fibonacci_series.rb
@@ -1,19 +1,27 @@
 class FibonacciSeries
   def initialize(string_inputs = ARGV[0])
-    IO.foreach(string_inputs) do |line|
-      max_fibonacci_index = line.to_i
-      prev_num = 0
-      curr_num = 1
-      fibonacci = 1
-
-      (max_fibonacci_index-1).times do
-        fibonacci = prev_num + curr_num
-        prev_num = curr_num
-        curr_num = fibonacci
+    File.read(string_inputs).split("\n").each do |line|
+      @max_fibonacci_index = line.to_i
+      if (@max_fibonacci_index>0)
+        puts calc_fibonacci
+      else
+        puts @max_fibonacci_index #because they always give you a blank line and expect zero at the end.
       end
-
-      puts fibonacci
     end
+  end
+
+  def calc_fibonacci
+    prev_num = 0
+    curr_num = 1
+    fibonacci = 1
+
+    (@max_fibonacci_index-1).times do
+      fibonacci = prev_num + curr_num
+      prev_num = curr_num
+      curr_num = fibonacci
+    end
+
+    fibonacci
   end
 end
 
