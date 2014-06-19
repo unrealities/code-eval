6bacca01328ca886955ad892c9c0d313b082aec3
tomszymanski <szymanski.tom@gmail.com>
2014-06-18 23:27:39 -0600

    Easy: 139: Working Experience: Ruby: Success

--- easy/139_WorkingExperience/ruby/lib/working_experience.rb
+++ easy/139_WorkingExperience/ruby/lib/working_experience.rb
@@ -1,6 +1,94 @@
+require 'date'
+
 class ParseInput
   def initialize(string_to_parse)
     @parsed_string = string_to_parse
+  end
+
+  def simple_date_array
+    return_array = @parsed_string.split(';')
+    return_array.map! do |dates|
+      dates.split('-')
+    end
+    return_array.map! do |dates|
+      dates.map!(&:strip)
+    end
+  end
+
+  def array_of_dates
+    simple_date_array.map! do |dates|
+      dates.map! do |date|
+        if dates.index(date) == 0
+          Date.parse(date)
+        else
+          first_of_the_month = Date.parse(date)
+          Date.new(first_of_the_month.year, first_of_the_month.month, -1)
+        end
+      end
+    end
+  end
+end
+
+class CalculateWorkingTime
+  def initialize(array_of_date_ranges)
+    @array_of_date_ranges = array_of_date_ranges
+    restart_compare_range_loop
+  end
+
+  def working_years
+    while @array_of_date_ranges.length > @current_date_range do
+      while @next_date_range < @array_of_date_ranges.length do
+        if overlap?([@array_of_date_ranges[@current_date_range],
+                     @array_of_date_ranges[@next_date_range]])
+          condense_date_ranges(@current_date_range, @next_date_range)
+          restart_compare_range_loop
+        else
+          @next_date_range += 1
+        end
+      end
+      @current_date_range += 1
+      increase_next_date_range
+    end
+
+    calc_years
+  end
+
+  private
+
+  def restart_compare_range_loop
+    @current_date_range = 0
+    increase_next_date_range
+  end
+
+  def increase_next_date_range
+    @next_date_range = @current_date_range + 1
+  end
+
+  def overlap?(array_of_date_ranges)
+    (array_of_date_ranges[1].min <= array_of_date_ranges[0].max &&
+    array_of_date_ranges[1].min >= array_of_date_ranges[0].min) ||
+    (array_of_date_ranges[0].min <= array_of_date_ranges[1].max &&
+    array_of_date_ranges[0].min >= array_of_date_ranges[1].min)
+  end
+
+  def condense_date_ranges(first_index_to_condense, second_index_to_condense)
+    flattened_dates = [@array_of_date_ranges[first_index_to_condense],
+                       @array_of_date_ranges[second_index_to_condense]].flatten
+    condensed_range = [flattened_dates.min, flattened_dates.max]
+    @array_of_date_ranges.delete_at(second_index_to_condense)
+    @array_of_date_ranges.delete_at(first_index_to_condense)
+    @array_of_date_ranges.unshift(condensed_range)
+  end
+
+  def calc_years
+    new_array = []
+
+    @array_of_date_ranges.each do |date_range|
+      new_array << (date_range[1]-date_range[0]).to_i
+    end
+
+    # needed to add '.round(1).truncate' to get all the cases to work.
+    (new_array.reduce(:+)/365.to_f).round(1).truncate
   end
 end
 
@@ -8,7 +96,9 @@ class WorkingExperience
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
       parsed_string = ParseInput.new(line.strip)
-      #doit
+      array_of_dates = parsed_string.array_of_dates
+      calc_work_time = CalculateWorkingTime.new(array_of_dates)
+      puts calc_work_time.working_years
     end
   end
 end
--- easy/139_WorkingExperience/ruby/spec/working_experience_spec.rb
+++ easy/139_WorkingExperience/ruby/spec/working_experience_spec.rb
@@ -2,6 +2,37 @@ require_relative 'spec_helper'
 
 RSpec.describe ParseInput do
   let(:parse_input) { ParseInput.new('Feb 2004-Dec 2009; Sep 2004-Jul 2008')}
+  describe '#simple_date_array' do
+    it 'should split array into an array of simple dates' do
+      expect(parse_input.simple_date_array).to eq [['Feb 2004','Dec 2009'], ['Sep 2004','Jul 2008']]
+    end
+  end
+
+  describe '#array_of_dates' do
+    let(:feb_04) {Date.new(2004,2)}
+    let(:dec_09) {Date.new(2009,12,-1)}
+    let(:sep_04) {Date.new(2004,9)}
+    let(:jul_08) {Date.new(2008,7,-1)}
+
+    it 'should return an array of date objects' do
+      expected_array = [[feb_04, dec_09], [sep_04, jul_08]]
+      expect(parse_input.array_of_dates).to eq expected_array
+    end
+  end
+end
+
+RSpec.describe CalculateWorkingTime do
+  let(:feb_04) {Date.new(2004,2)}
+  let(:dec_09) {Date.new(2009,12,-1)}
+  let(:sep_04) {Date.new(2004,9)}
+  let(:jul_08) {Date.new(2008,7,-1)}
+  let(:calc_work_time) { CalculateWorkingTime.new([[feb_04, dec_09], [sep_04, jul_08]])}
+
+  describe '#working_years' do
+    it 'should return working years given an array of date ranges' do
+      expect(calc_work_time.working_years).to eq 5
+    end
+  end
 end
 
 RSpec.describe WorkingExperience do
@@ -16,11 +47,35 @@ RSpec.describe WorkingExperience do
   describe "#initialize" do
     it "should return correct test outputs given test input" do
       WorkingExperience.new('../test_input.txt')
+      expect($stdout.string).to match('14')
+      expect($stdout.string).to match('19')
+      expect($stdout.string).to match('9')
+      expect($stdout.string).to match('13')
       expect($stdout.string).to match('5')
       expect($stdout.string).to match('4')
       expect($stdout.string).to match('1')
       expect($stdout.string).to match('6')
       expect($stdout.string).to match('0')
+      expect($stdout.string).to match('5')
+      expect($stdout.string).to match('4')
+      expect($stdout.string).to match('1')
+      expect($stdout.string).to match('6')
+      expect($stdout.string).to match('0')
+      expect($stdout.string).to match('11')
+      expect($stdout.string).to match('7')
+      expect($stdout.string).to match('5')
+      expect($stdout.string).to match('0')
+      expect($stdout.string).to match('8')
+      expect($stdout.string).to match('20')
+      expect($stdout.string).to match('5')
+      expect($stdout.string).to match('18')
+      expect($stdout.string).to match('12')
+      expect($stdout.string).to match('7')
+      expect($stdout.string).to match('5')
+      expect($stdout.string).to match('7')
+      expect($stdout.string).to match('10')
+      expect($stdout.string).to match('4')
+      expect($stdout.string).to match('17')
     end
   end
 end
