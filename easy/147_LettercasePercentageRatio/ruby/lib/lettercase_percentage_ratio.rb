3258533d37244473893eb60b8859d7a5f258da7c
tomszymanski <szymanski.tom@gmail.com>
2014-06-21 00:31:11 -0600

    Easy: 147: Lettercase Percentage Ratio: Ruby: Success

--- easy/147_LettercasePercentageRatio/ruby/lib/lettercase_percentage_ratio.rb
+++ easy/147_LettercasePercentageRatio/ruby/lib/lettercase_percentage_ratio.rb
@@ -1,6 +1,34 @@
 class ParseInput
   def initialize(string_to_parse)
     @parsed_string = string_to_parse
+    @length = @parsed_string.length * 1.00
+    @uppercase_count = 0
+    @lowercase_count = 0
+    get_counts
+  end
+
+  def uppercase_percent
+    ((@uppercase_count / @length) * 100.0)
+  end
+
+  def lowercase_percent
+    ((@lowercase_count / @length) * 100.0)
+  end
+
+  private
+
+  def get_counts
+    @parsed_string.each_char do |char|
+      if uppercase?(char)
+        @uppercase_count += 1
+      else
+        @lowercase_count += 1
+      end
+    end
+  end
+
+  def uppercase?(char)
+    char.upcase == char
   end
 end
 
@@ -8,7 +36,7 @@ class LettercasePercentageRatio
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
       parsed_string = ParseInput.new(line.strip)
-      # do it
+      puts "lowercase: %.2f" % parsed_string.lowercase_percent + " uppercase: %.2f" % parsed_string.uppercase_percent
     end
   end
 end
--- easy/147_LettercasePercentageRatio/ruby/spec/lettercase_percentage_ratio_spec.rb
+++ easy/147_LettercasePercentageRatio/ruby/spec/lettercase_percentage_ratio_spec.rb
@@ -2,6 +2,17 @@ require_relative 'spec_helper'
 
 RSpec.describe ParseInput do
   let(:parse_input) { ParseInput.new('thisTHIS') }
+
+  describe '#uppercase_percent' do
+    it 'should return the value of uppercase characters' do
+      expect(parse_input.uppercase_percent).to eq 50.0
+    end
+  end
+  describe '#lowercase_percent' do
+    it 'should return the value of lowercase characters' do
+      expect(parse_input.lowercase_percent).to eq 50.0
+    end
+  end
 end
 
 RSpec.describe LettercasePercentageRatio do
@@ -16,8 +27,8 @@ RSpec.describe LettercasePercentageRatio do
   describe '#initialize' do
     it 'should return correct test outputs given test input' do
       LettercasePercentageRatio.new('../test_input.txt')
-      expect($stdout.string).to match('lowercase: 20.00 uppercase: 80.00')
       expect($stdout.string).to match('lowercase: 50.00 uppercase: 50.00')
+      expect($stdout.string).to match('lowercase: 20.00 uppercase: 80.00')
       expect($stdout.string).to match('lowercase: 0.00 uppercase: 100.00')
       expect($stdout.string).to match('lowercase: 33.33 uppercase: 66.67')
     end
