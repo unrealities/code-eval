69001d6b5641a98dd92f9bcb6194350323afb82c
Tom Szymanski <szymanski.tom@gmail.com>
2015-02-01 23:15:40 -0700

    Easy: Ruby: 178 - Matrix Rotation: Success

--- easy/178_MatrixRotation/ruby/lib/matrix_rotation.rb
+++ easy/178_MatrixRotation/ruby/lib/matrix_rotation.rb
@@ -1,14 +1,64 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @parsed_string = string_to_parse
+class SquareMatrix
+  attr_reader :dim
+
+  def initialize(initial_values)
+    init_array_representation = initial_values.split(' ')
+    @dim = Math.sqrt(init_array_representation.length).to_i
+    @matrix = matrix(init_array_representation)
   end
+  
+  def matrix(array_representation)
+    matrix = Array.new(@dim) { Array.new(@dim)}
+    col_index = -1
+    row_index = -1
+    array_index = -1
+    @dim.times do
+      col_index += 1
+      @dim.times do
+        row_index += 1
+        array_index += 1
+        matrix[col_index][row_index] = array_representation[array_index]
+      end
+      row_index = -1
+    end
+    matrix
+  end
+  
+  def rotate_90_cw
+    rotated_matrix = Array.new(@dim) { Array.new(@dim)}
+    @matrix.each_with_index do |col, col_index|
+      col.each_index do |row_index|
+        rotated_row_index = col_index
+        rotated_col_index = @dim - 1 - row_index
+        rotated_matrix[col_index][row_index] = @matrix[rotated_col_index][rotated_row_index]
+      end
+    end
+    @matrix = rotated_matrix
+    self
+  end
+  
+  def to_s
+    output = ''
+    col_index = -1
+    row_index = -1
+    @dim.times do
+      col_index += 1
+      @dim.times do
+        row_index += 1
+        output += @matrix[col_index][row_index] + ' '
+      end
+      row_index = -1
+    end
+    output.rstrip
+  end
+  
 end
 
 class MatrixRotation
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      # do work
+      matrix = SquareMatrix.new(line.strip)
+      puts matrix.rotate_90_cw.to_s
     end
   end
 end
--- easy/178_MatrixRotation/ruby/spec/matrix_rotation_spec.rb
+++ easy/178_MatrixRotation/ruby/spec/matrix_rotation_spec.rb
@@ -1,8 +1,17 @@
 require_relative 'spec_helper'
 
-RSpec.describe ParseInput do
-  let(:parse_input) { ParseInput.new('a b c d') }
-
+RSpec.describe SquareMatrix do
+  describe '#initialize' do
+    it 'has a dimension equal to the square root of the length of the initial values' do
+      expect(SquareMatrix.new('a b c d').dim).to eq(2)
+    end
+  end
+  
+  describe '#to_s' do
+    it 'returns a string representation of the matrix' do
+      expect(SquareMatrix.new('a b c d').to_s).to eq('a b c d')
+    end
+  end
 end
 
 RSpec.describe MatrixRotation do
@@ -17,8 +26,17 @@ RSpec.describe MatrixRotation do
   describe '#initialize' do
     it 'should return correct test outputs given test input' do
       MatrixRotation.new('../test_input.txt')
+      # [ a b ] -> [ c a ]
+      # [ c d ] -> [ d b ]
       expect($stdout.string).to match('c a d b')
+      # [ a b c d ] -> [ m i e a ]
+      # [ e f g h ] -> [ n j f b ]
+      # [ i j k l ] -> [ o k g c ]
+      # [ m n o p ] -> [ p l h d ]
       expect($stdout.string).to match('m i e a n j f b o k g c p l h d')
+      # [ a b c ] -> [ g d a ]
+      # [ d e f ] -> [ h e b ]
+      # [ g h i ] -> [ i f c ]
       expect($stdout.string).to match('g d a h e b i f c')
     end
   end
