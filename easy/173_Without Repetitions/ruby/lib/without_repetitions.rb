af4af2b7000df00ff36110a685122cad6a6e7fd8
Tom Szymanski <szymanski.tom@gmail.com>
2015-02-03 09:34:21 -0700

    Easy: 173: Without Repititions: Ruby: Success

--- easy/173_Without Repetitions/ruby/lib/without_repetitions.rb
+++ easy/173_Without Repetitions/ruby/lib/without_repetitions.rb
@@ -1,16 +1,18 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @parsed_string = string_to_parse
-  end
-end
-
 class WithoutRepetitions
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      # do work
+      string_array = line.strip.split('')
+      i = 0
+      while i < string_array.length do
+        if string_array[i] == string_array[i+1]
+          string_array.delete_at(i+1)
+          i -= 1
+        end
+        i += 1
+      end
+      puts string_array.join('')
     end
   end
 end
 
-# WithoutRepetitions.new
+WithoutRepetitions.new
