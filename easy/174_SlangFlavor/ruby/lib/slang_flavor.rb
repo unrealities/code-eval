e7729edcb8e446daaaeb5304acbf44e1360a5fc3
Tom Szymanski <szymanski.tom@gmail.com>
2015-02-02 23:40:04 -0700

    Easy: 174: Slang Flavor: Success

--- easy/174_SlangFlavor/ruby/lib/slang_flavor.rb
+++ easy/174_SlangFlavor/ruby/lib/slang_flavor.rb
@@ -1,28 +1,26 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @parsed_string = string_to_parse
-  end
-end
-
 class SlangFlavor
   SLANGS = [
-    ', yeah!',
-    ', this is crazy, I tell ya.',
-    ', can U believe this?',
-    ', eh?',
-    ', aw yea.',
-    ', yo.',
-    '? No way!',
-    '. Awesome!'
+      ', yeah!',
+      ', this is crazy, I tell ya.',
+      ', can U believe this?',
+      ', eh?',
+      ', aw yea.',
+      ', yo.',
+      '? No way!',
+      '. Awesome!'
   ]
-  PUNCTUATIONS = %w{. ! ?}
 
   def initialize(string_inputs = ARGV[0])
+    i = 0
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      # do work
+      output = line.strip.gsub(/[?!.]/) do |str|
+        i += 1
+        i = 1 if i == ( SLANGS.length * 2 + 1 )
+        i.even? ? SLANGS[ i/2 - 1 ] : str
+      end
+      puts output
     end
   end
 end
 
-# SlangFlavor.new
+#SlangFlavor.new
--- easy/174_SlangFlavor/ruby/spec/slang_flavor_spec.rb
+++ easy/174_SlangFlavor/ruby/spec/slang_flavor_spec.rb
@@ -1,10 +1,5 @@
 require_relative 'spec_helper'
 
-RSpec.describe ParseInput do
-  let(:parse_input) { ParseInput.new('a b c d') }
-
-end
-
 RSpec.describe SlangFlavor do
   before do
     $stdout = StringIO.new
@@ -18,8 +13,7 @@ RSpec.describe SlangFlavor do
     it 'should return correct test outputs given test input' do
       SlangFlavor.new('../test_input.txt')
       expect($stdout.string).to match('Lorem ipsum dolor sit amet. Mea et habeo doming praesent, yeah! Te inani utroque recteque has, sea ne fugit verterem!')
-      expect($stdout.string).to match('Usu ei scripta phaedrum, an sed salutatus definiebas, this is
-crazy, I tell ya. Qui ut recteque gloriatur reformidans. Qui solum aeque sapientem cu, can U believe this?')
+      expect($stdout.string).to match('Usu ei scripta phaedrum, an sed salutatus definiebas, this is crazy, I tell ya. Qui ut recteque gloriatur reformidans. Qui solum aeque sapientem cu, can U believe this?')
       expect($stdout.string).to match('Eu nam nusquam quaestio principes.')
     end
   end
