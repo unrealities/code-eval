239d2f8f82ed6f1ab9e65f9ccf4cb1bd818c7f38
Tom Szymanski <szymanski.tom@gmail.com>
2015-02-04 23:50:24 -0700

    Easy: 166: Delta Time: Success

--- easy/166_DeltaTime/ruby/lib/delta_time.rb
+++ easy/166_DeltaTime/ruby/lib/delta_time.rb
@@ -1,14 +1,13 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @parsed_string = string_to_parse
-  end
-end
+require 'time'
 
 class DeltaTime
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      # do work
+      time_array = line.strip.split(' ')
+      time_1 = Time.parse(time_array[0]).utc
+      time_2 = Time.parse(time_array[1]).utc
+      diff = Time.at((time_1 - time_2).abs).utc
+      puts diff.strftime('%H:%M:%S')
     end
   end
 end
