16ee0396fdc04308acffbf6a548748754b183004
Tom Szymanski <szymanski.tom@gmail.com>
2015-02-05 23:18:25 -0700

    Easy: 152: Age Distribution: Success

--- easy/152_AgeDistribution/ruby/lib/age_distribution.rb
+++ easy/152_AgeDistribution/ruby/lib/age_distribution.rb
@@ -1,16 +1,29 @@
-class ParseInput
-  def initialize(string_to_parse)
-    @parsed_string = string_to_parse
-  end
-end
-
 class AgeDistribution
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
-      parsed_string = ParseInput.new(line.strip)
-      # do work
+      age = line.strip.to_i
+      case age
+      when 0,1,2
+        puts 'Still in Mama\'s arms'
+      when 3,4
+        puts 'Preschool Maniac'
+      when 5..11
+        puts 'Elementary school'
+      when 12..14
+        puts 'Middle school'
+      when 15..18
+        puts 'High school'
+      when 19..22
+        puts 'College'
+      when 23..65
+        puts 'Working for the man'
+      when 66..100
+        puts 'The Golden Years'
+      else
+        puts 'This program is for humans'
+      end
     end
   end
 end
 
-AgeDistribution.new
+# AgeDistribution.new
