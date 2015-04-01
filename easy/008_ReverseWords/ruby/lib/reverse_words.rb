f39c4a41b810ad4dc64cb5a7c4ee862249592336
Tom Szymanski <szymanski.tom@gmail.com>
2015-04-01 08:03:01 -0600

    Easy: 008: ReverseWords: Ruby: Success

--- easy/008_ReverseWords/ruby/lib/reverse_words.rb
+++ easy/008_ReverseWords/ruby/lib/reverse_words.rb
@@ -1,7 +1,7 @@
 class ReverseWords
   def initialize(string_inputs = ARGV[0])
     IO.foreach(string_inputs) do |line|
-      # do work
+      puts line.split(" ").reverse.join(" ")
     end
   end
 end
