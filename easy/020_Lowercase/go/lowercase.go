0430d5d5c9228871b88c632c906a7d44308a3289
Tom Szymanski <szymanski.tom@gmail.com>
2015-04-05 09:24:34 -0600

    Easy: 020: Lowercase: Go: Success

---
+++ easy/020_Lowercase/go/lowercase.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(Lowercase(scanner.Text()))
+	}
+}
+
+func Lowercase(s string) string {
+	return strings.ToLower(s)
+}
---
+++ easy/020_Lowercase/go/lowercase_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "bufio"
+import "log"
+import "os"
+
+import "testing"
+
+func TestLowercase(t *testing.T) {
+	expect := [2]string{
+		"hello codeeval",
+		"this is some text",
+	}
+	inputs := Inputs("../test_input.txt")
+
+	for key, value := range expect {
+		result := Lowercase(inputs[key])
+		if value != result {
+			t.Error("Expected: " + value + " | Got: " + result)
+		}
+	}
+}
+
+func Inputs(filePath string) [2]string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	inputs := [2]string{}
+	i := 0
+
+	for scanner.Scan() {
+		inputs[i] = scanner.Text()
+		i++
+	}
+
+	return inputs
+}
